bc91c0e046320537e80de0e43ac46ae6b5b9e33d
ziyanda8611 <ziyanda8611@gmail.com>
2026-05-17 19:37:45 +0200

    Delete pages directory

--- pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { BasePage } from './BasePage';
-export { AdminLoginPage } from './AdminLoginPage';
-export { AdminPanelPage } from './AdminPanelPage';
-export { EnrollmentsPage } from './EnrollmentsPage';
-export { StudentLoginPage } from './StudentLoginPage';
